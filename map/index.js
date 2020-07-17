a9f54851a6811318bcca1086466740861fab11bc
daniltirsk <56964428+daniltirsk@users.noreply.github.com>
2020-07-17 12:36:19 +0800

    final version(well not really)

--- map/index.js
+++ map/index.js
@@ -1,102 +1,147 @@
 const http = require('http');
 const fs = require("fs");
 const path = require("path");
+const express = require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
+var grid = fs.readFileSync("field.json");
+grid = JSON.parse(grid);
+var gridChanges = fs.readFileSync("fieldChanges.json");
+gridChanges = JSON.parse(gridChanges);
 
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.text())
 
-const gridX = 8
-const gridY = 10
+var rawParser = bodyParser.raw();
 
-var gridArr = [];
-
-
-for (var i = 0; i < gridY; i++) {
-    nArr = [];
-    for (var j = 0; j < gridX; j++) {
-        nArr.push(null);
-    }
-    gridArr.push(nArr);
-}
+const gridX = 8;
+const gridY = 10;
 
 // Массив хронящий изменения основного поля
 
-var gridArrChanges = [];
-
-for (var i = 0; i < gridY; i++) {
-    nArr = [];
-    for (var j = 0; j < gridX; j++) {
-        nArr.push(null);
-    }
-    gridArrChanges.push(nArr);
-}
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 
+app.get('/', (req, res) => {
+    res.send('Hello from Express!')
+})
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-
-  if (req.url === "/"){
-  	// fs.readFile(path.join(__dirname,'main.html'), (err,data) => {
-  	// 	if (err) {
-  	// 		throw err;
-  	// 	}
-  	// 	res.statusCode = 200;
-  	// 	res.setHeader('Content-Type', 'text/html');
-  	// 	  res.end("hello");
-  	// });
-  	
-  		res.statusCode = 200;
-  		res.setHeader("Access-Control-Allow-Origin", "*");
-  		res.setHeader('Content-Type', 'text/plain');
-  		res.end("hello");
+app.get('/getMarkersPosition', (req, res) => {
+  	res.json(grid);
   }
+ )
 
-  if (req.url === "/getMarkersPosition"){
-  	res.statusCode = 200;
-  	res.setHeader("Access-Control-Allow-Origin", "*");
-  	res.setHeader('Content-Type', 'text/json');
-  	var filledCells = []
-  	var cellsNum = getRandomInt(80);
+app.get('/getRecentChanges', (req, res) => {
+  	res.json(gridChanges);
+  	gridChanges = [];
+  	fs.writeFileSync('fieldChanges.json', JSON.stringify(gridChanges), 'utf8');
+  }
+ )
 
-  	for (var i = 0; i < gridY; i++) {
-	    for (var j = 0; j < gridX; j++) {
-	        gridArr[i][j] = null;
+app.get('/getFrontGridImg', (req, res) => {
+    res.sendFile('C:/Users/ASUS/Documents/GitHub/PracticeFirstCourseAppliedInformatics/map/out.png');
+})
+
+
+app.post("/updateFieldAdd",(req, res) => {
+	console.log('add',req.body);
+	if (req.body.x != null && req.body.y != null && req.body.id != null && req.body.r != null){
+		var add = {
+	    	x:req.body.x,
+	    	y:req.body.y,
+	    	id:req.body.id,
+	    	r:req.body.r
 	    }
+	    grid.push(add);
+	    fs.writeFileSync('field.json', JSON.stringify(grid), 'utf8');
+	    res.status(200).send();
+	} else {
+		res.status(400).send("Wrong request, provide x,y,id,r");
 	}
+})
 
-  	for (var i = cellsNum - 1; i >= 0; i--) {
-  		var newCell = getRandomInt(80);
-  		gridArr[newCell%gridY][Math.floor(newCell/gridY)] = [];
-        gridArr[newCell%gridY][Math.floor(newCell/gridY)].push(getRandomInt(6),0);
-  	}
+app.post("/updateFieldRemove",(req, res) => {
+	console.log('deletion',req.body);
+	if (req.body.x != null && req.body.y != null){
+		var del = {
+	    	x:req.body.x,
+	    	y:req.body.y
+	    }
 
-  	var response = [];
-    for (var i = 0; i < gridY; i++) {
-        for (var j = 0; j < gridX; j++) {
-            if(gridArr[i][j]){
-                response.push({
-                    x: j,
-                    y: i,
-                    id: gridArr[i][j][0]
-                });
-            }
-        }
+	    grid = grid.filter(obj => obj.x != del.x || obj.y != del.y);
+
+	    fs.writeFileSync('field.json', JSON.stringify(grid), 'utf8');
+
+	    res.status(200).send();
+	} else {
+		res.status(400).send("Wrong request, provide x,y");
+	}
+})
+
+app.post("/updateFieldRotate",(req, res) => {
+	console.log('rotation',req.body);
+	if (req.body.x != null && req.body.y != null && req.body.r != null){
+		var rot = {
+	    	x:req.body.x,
+	    	y:req.body.y,
+	    	r:req.body.r
+	    }
+	    grid.find(obj => obj.x == rot.x && obj.y == rot.y).r = rot.r;
+	    fs.writeFileSync('field.json', JSON.stringify(grid), 'utf8');
+	    res.status(200).send();
+	} else {
+		res.status(400).send("Wrong request, provide x,y,r");
+	}
+})
+
+app.post("/updateFieldChanges",(req, res) => {
+	console.log('change',req.body);
+	if (req.body.x != null && req.body.y != null && req.body.typeOfChange != null){
+		var change = {
+	    	x:req.body.x,
+	    	y:req.body.y,
+	    	typeOfChange:req.body.typeOfChange
+	    }
+
+	    var oldChange = gridChanges.find(obj => obj.x == change.x && obj.y == change.y);
+
+	    if (oldChange){
+	    	gridChanges.find(obj => obj.x == change.x && obj.y == change.y).typeOfChange = change.typeOfChange;
+	    } else {
+	    	gridChanges.push(change);
+	    }
+
+	    fs.writeFileSync('fieldChanges.json', JSON.stringify(gridChanges), 'utf8');
+
+	    res.status(200).send();
+	} else {
+		res.status(400).send("Wrong request, provide x,y,typeOfChange");
+	}
+})
+
+app.post("/uploadFrontImg",(req, res) => {
+	console.log('img');
+	if (req.body){
+		var base64Data = req.body.replace('data:image/png;base64,','');
+		fs.writeFile("out.png", base64Data, 'base64', function(err) {
+		  console.log(err);
+		});
+		res.status(200).send();
+	} else {
+		res.status(400).send("Wrong request, provide x,y,typeOfChange");
+	}
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
     }
-    response = JSON.stringify(response);
-
-  	res.end(response);
-  }
-
-
-
-  });
-
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`server is listening on ${port}`)
+})
--- map/script.js
+++ map/script.js
@@ -12,7 +12,6 @@ const gridY = 10
 
 var lastClicked;
 var lastClickedType;
-
 var gridArr = [];
 
 var flipped = false;
@@ -47,7 +46,6 @@ class Tile {
         this.name = name;
         this.markerID = markerID;
     }
-
 }
 
 // Маркеры
@@ -115,7 +113,7 @@ function gridFront(evt) {
         gridArr[el.className%gridY][Math.floor(el.className/gridY)] = [];
         gridArr[el.className%gridY][Math.floor(el.className/gridY)].push(lastClickedType,0);
         // добавляем что в данной ячейке появился новый маркер
-        gridArrChanges[el.className%gridY][Math.floor(el.className/gridY)] = 'new marker';
+        updateFieldChanges(Math.floor(el.className/gridY),el.className%gridY,'new marker');
         // добавляем маркер на нижнюю доску
         var backCellNumber = +el.className + gridY - 1 - el.className%gridY*2;
         var backCell = document.getElementsByClassName(backCellNumber);
@@ -126,8 +124,13 @@ function gridFront(evt) {
                 backCell[i].style.transform = 'rotate(0deg)';
             }
         }
+        addMarkerToServ(Math.floor(el.className/gridY),el.className%gridY,lastClickedType.markerID,0);
+
     };
     // перерисовываем канвас
+
+    uploadFrontGridImg();
+
     changeCanv();
     //console.log(gridArr);
 };
@@ -146,7 +149,7 @@ function gridFrontRemove(evt) {
 
     gridArr[el.className%gridY][Math.floor(el.className/gridY)] = null;
     // произошло событие удаления, отмечаем это в массиве
-    gridArrChanges[el.className%gridY][Math.floor(el.className/gridY)] = 'marker deleted';
+    updateFieldChanges(Math.floor(el.className/gridY),el.className%gridY,'marker deleted');
     //удаление с нижнего поля
     var backCellNumber = +el.className + gridY - 1 - el.className%gridY*2;
     var backCell = document.getElementsByClassName(backCellNumber);
@@ -156,6 +159,10 @@ function gridFrontRemove(evt) {
             backCell[i].firstChild.remove();
         }
     }
+
+    deleteMarkerFromServ(Math.floor(el.className/gridY),el.className%gridY);
+
+    uploadFrontGridImg();
     //перерисуем канвас
     changeCanv()
 
@@ -169,6 +176,7 @@ function gridFrontRotate(evt) {
     var backCellNumber = +el.parentElement.className + gridY - 1 - el.parentElement.className%gridY*2;
     var backCell = document.getElementsByClassName(backCellNumber);
     var targetBackCell;
+    var r;
 
     for (var i = 0; i < backCell.length; i++) {
         if (backCell[i].parentNode.parentNode.classList.contains("gridBack")) {
@@ -180,25 +188,33 @@ function gridFrontRotate(evt) {
         case 'rotate(0deg)':
             el.style.transform = 'rotate(90deg)';
             targetBackCell.style.transform = 'rotate(90deg)';
-            gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 1;
+            //gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 1;
+            r = 1;
             break;
         case 'rotate(90deg)':
             el.style.transform = 'rotate(180deg)';
             targetBackCell.style.transform = 'rotate(180deg)';
-            gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 2;
+            //gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 2;
+            r = 2;
             break;
         case 'rotate(180deg)':
             el.style.transform = 'rotate(270deg)';
             targetBackCell.style.transform = 'rotate(270deg)';
-            gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 3;
+            //gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 3;
+            r = 3;
             break;
         default:
             el.style.transform = 'rotate(0deg)';
             targetBackCell.style.transform = 'rotate(0deg)';
-            gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 0;
+            //gridArr[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)][1] = 0;
+            r = 0;
     }
     //произошел поворот ячейки, отмечаем это в массиве
-    gridArrChanges[el.parentElement.className%gridY][Math.floor(el.parentElement.className/gridY)] = 'marker rotated';
+    updateFieldChanges(Math.floor(el.parentElement.className/gridY),el.parentElement.className%gridY,'marker rotated');
+
+    updateMarkerRotationServ(Math.floor(el.parentElement.className/gridY),el.parentElement.className%gridY,r);
+
+    uploadFrontGridImg();
     //перерисовываем канвас
     changeCanv();
     //console.log(gridArr);
@@ -222,7 +238,7 @@ function gridSelect(evt, i) {
 window.addEventListener('click', function (evt) {
 
     //обработка слайдера
-    if (evt.target.tagName === "INPUT"){
+    if (evt.target.id === "field"){
          fieldRotate()
     }
     if (evt.target.tagName === "IMG") {
@@ -321,30 +337,37 @@ function eraseAll(){
 
 
 
+function AddMarkerToBoard(x,y,id,r) {
+    let cells = document.getElementsByClassName(x*gridY + y);
+    var el;
+    lastClickedType = tileTypes[id];
+
+    for (var k = cells.length - 1; k >= 0; k--) {
+        if (cells[k].parentElement.parentElement.classList.contains("gridFront")){
+            el = cells[k];
+        }
+    }
+
+    el.innerHTML = '<div class="flip-card"> <div class="flip-card-inner"> <div class="flip-card-front"> <img src=".' + lastClickedType.front + '"> </div> <div class="flip-card-back"> <img src=".' + lastClickedType.back + '"> </div> </div> </div>'
+    el.firstChild.style.transform = 'rotate('+r*90+'deg)';
+    // если поле перевернуто, перевернуть маркер перед добавлением
+    if (flipped){
+        el.firstChild.children[0].classList.add('flipped');
+    }
+        // добавляем что в данной ячейке появился новый маркер
+        // добавляем маркер на нижнюю доску
+    var backCellNumber = +el.className + gridY - 1 - el.className%gridY*2;
+    var backCell = document.getElementsByClassName(backCellNumber);
+
+    for (var i = 0; i < backCell.length; i++) {
+        if (backCell[i].parentNode.parentNode.classList.contains("gridBack")) {
+            backCell[i].innerHTML = "<img src=." + lastClickedType.back + ">";
+            backCell[i].style.transform = 'rotate(0deg)';
+        }
+    };
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// эксперементальные кнопки для эксперементальных функций (просто чтоб функции тестить)
-
-
- var tr1 = document.getElementById("try").addEventListener("click", try2);
- var tr2 = document.getElementById("try2").addEventListener("click", try1);
 
 
 
@@ -352,34 +375,16 @@ var canv;
 var canvCont;
 var matrix;
 // эксперементальная функция(аффинные преобразования)
-function try2(){
-    matrix = new Matrix(canvCont);
-    canvCont.clearRect(0,0,472,378);
-    matrix.rotate(0.7);
-}
-function try1() {
-      var canvas = document.getElementById('canvas');
-      console.log(canvas);
-      if (canvas.getContext) {
-        console.log('hey');
-        var ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(10, 10, 50, 50);
-
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(30, 30, 50, 50);
-      }
-    }
 
 
 // изменение канваса(на данный момент просто его пересоздание)
 function changeCanv(){
     html2canvas(document.getElementById("gridBack")).then(function(canvas) {
-    canvCont = document.getElementById('canvas').getContext('2d');
-    // canvCont.imageSmoothingEnabled = false;
-    canvCont.drawImage(canvas,0,0);
-    canv = canvas;
+        canvCont = document.getElementById('canvas').getContext('2d');
+        // canvCont.imageSmoothingEnabled = false;
+        canvCont.drawImage(canvas,0,0);
+        canv = canvas;
     });
 }
 
@@ -396,69 +401,155 @@ function getImg(){
   link.click();
 }
 
+// функции отправляющие данные на сервер
 
 
+async function addMarkerToServ(xn,yn,idn,rn){
+    let newMark = {
+        x:xn,
+        y:yn,
+        id:idn,
+        r:rn
+    }
+    console.log(newMark);
+    let response = await fetch("http://localhost:3000/updateFieldAdd", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(newMark)
+    });
+}
 
-// функции для получения данных с цифрового двойника
+async function deleteMarkerFromServ(xn,yn){
+    let delMark = {
+        x:xn,
+        y:yn
+    }
+    console.log(delMark);
+    let response = await fetch("http://localhost:3000/updateFieldRemove", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(delMark)
+    });
+}
+
+async function updateMarkerRotationServ(xn,yn,rn){
+    let rotMark = {
+        x:xn,
+        y:yn,
+        r:rn
+    }
+    console.log(rotMark);
+    let response = await fetch("http://localhost:3000/updateFieldRotate", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(rotMark)
+    });
+}
+
+async function updateMarkerRotationServ(xn,yn,rn){
+    let rotMark = {
+        x:xn,
+        y:yn,
+        r:rn
+    }
+    console.log(rotMark);
+    let response = await fetch("http://localhost:3000/updateFieldRotate", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(rotMark)
+    });
+}
+
+async function updateFieldChanges(xn,yn,typeOfChangen){
+    let change = {
+        x:xn,
+        y:yn,
+        typeOfChange:typeOfChangen
+    }
+    console.log(change);
+    let response = await fetch("http://localhost:3000/updateFieldChanges", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(change)
+    });
+}
+
+async function loadField(){
+    let response = await fetch("http://localhost:3000/getMarkersPosition");
+    if (response.ok) { 
+      let json = await response.json();
+      for (var i = json.length - 1; i >= 0; i--) {
+        AddMarkerToBoard(json[i].x,json[i].y,json[i].id,json[i].r);
+      }
+      changeCanv();
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+ }
+
+async function uploadFrontGridImg(){
+    var backs = document.getElementsByClassName("flip-card-back");
+    for (var i = backs.length - 1; i >= 0; i--) {
+        backs[i].style.display = 'none'
+    }
+    html2canvas(document.getElementById("gridFront")).then(async function(canvas) {
+        let response = await fetch("http://localhost:3000/uploadFrontImg", {
+        method: 'POST',
+        'Content-Type':'text/plain;charset=utf-8',
+        body: canvas.toDataURL('image/png',1.0)
+        });
+    });
+    for (var i = backs.length - 1; i >= 0; i--) {
+        backs[i].style.display = 'block';
+    }
+}
+
+loadField();
+
+
+// функции для получения данных с сервера
 
 
 async function f0(){
-    console.log(0);
-    // var response = [];
-    // for (var i = 0; i < gridY; i++) {
-    //     for (var j = 0; j < gridX; j++) {
-    //         if(gridArr[i][j]){
-    //             response.push({
-    //                 x: j,
-    //                 y: i,
-    //                 id: gridArr[i][j][0].markerID
-    //             });
-    //         }
-    //     }
-    // }
-    // response = JSON.stringify(response);
-    // console.log(response);
-
-
-    let response = await fetch("http://localhost:3000");
-if (response.ok) { // если HTTP-статус в диапазоне 200-299
-  // получаем тело ответа (см. про этот метод ниже)
-  let json = await response.text();
-  console.log(response.status);
-  console.log(json);
-} else {
-  alert("Ошибка HTTP: " + response.status);
-}
- }
-
-  async function f1(){
-    console.log(1);
-    // var response = [];
-    // for (var i = 0; i < gridY; i++) {
-    //     for (var j = 0; j < gridX; j++) {
-    //         if(gridArr[i][j]){
-    //             response.push({
-    //                 x: j,
-    //                 y: i,
-    //                 r: gridArr[i][j][1]
-    //             });
-    //         }
-    //     }
-    // }
-    // response = JSON.stringify(response);
-    // console.log(response);
     let response = await fetch("http://localhost:3000/getMarkersPosition");
-    if (response.ok) { // если HTTP-статус в диапазоне 200-299
-  // получаем тело ответа (см. про этот метод ниже)
-  let json = await response.json();
-  console.log(response.status);
-  console.log(json);
-} else {
-  alert("Ошибка HTTP: " + response.status);
-}
+    if (response.ok) { 
+      let json = await response.json();
+      console.log(response.status);
+      console.log(json);
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
  }
+
+async function f1(){
+    console.log(0);
+    let response = await fetch("http://localhost:3000/getFrontGridImg");
+    if (response.ok) {
+      console.log(response.status);
+      console.log(response);
+      var res = await response.blob();
+      var objectURL = URL.createObjectURL(res);
+      var link = document.createElement('a');
+      link.download = 'FrontGrid.png';
+      link.href = objectURL;
+      link.click();
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+ }
+
 //функция не работает если запускать сайт из файловой системы, запускай через сервер (localhost)
-   function f2(){
+function f2(){
     console.log(2);
     var backs = document.getElementsByClassName("flip-card-back");
     for (var i = backs.length - 1; i >= 0; i--) {
@@ -480,28 +571,19 @@ if (response.ok) { // если HTTP-статус в диапазоне 200-299
     console.log(3);
  }
 
-  function f4(){
-    console.log(4);
+ async function f4(){
+     let response = await fetch("http://localhost:3000/getRecentChanges");
+    if (response.ok) { 
+      let json = await response.json();
+      console.log(response.status);
+      console.log(json);
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
  }
 
   function f5(){
-    console.log(5);
-    var response = [];
-    console.log(gridArrChanges);
-    for (var i = 0; i < gridY; i++) {
-        for (var j = 0; j < gridX; j++) {
-            if(gridArrChanges[i][j]){
-                response.push({
-                    x: j,
-                    y: i,
-                    typeOfChange: gridArrChanges[i][j]
-                });
-                gridArrChanges[i][j] = null;
-            }
-        }
-    }
-    response = JSON.stringify(response);
-    console.log(response);
+    
 
  }
 
@@ -521,37 +603,91 @@ if (response.ok) { // если HTTP-статус в диапазоне 200-299
 
 // Функции визуальных эффектов
 
- function e0(){
-    canv.style.filter = "blur(1px)";
-}
-
- function e1(){
-    canv.style.filter = "blur(1px)";
-}
- function e2(){
-    canv.style.filter = "blur(1px)";
-}
- function e3(){
-    canv.style.filter = "blur(1px)";
-}
- function e4(){
-    canv.style.filter = "blur(1px)";
-}
- function e5(){
-    canv.style.filter = "blur(1px)";
-}
- function e6(){
-    canv.style.filter = "blur(1px)";
-}
 
 
 // привязываем функции к кнопкам
  var effects = [e0,e1,e2,e3,e4,e5,e6]
 
-  var ebuttons = document.querySelectorAll(".buttons .effects .btn");
+  var ebuttons = document.querySelectorAll(".effBtn a");
 
   for (var i = ebuttons.length - 1; i >= 0; i--) {
      ebuttons[i].addEventListener("click", effects[i]);
  }
 
 
+function showEffectsMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+ function e0(){
+    // canvCont.resetTransform();
+    var deg = document.getElementById("i0").value;
+    canvCont.clearRect(0,0,472,378);
+    canvCont.translate(236,189);
+    canvCont.rotate(deg * Math.PI / 180);
+    // canvCont.scale(0.78,0.78)
+    canvCont.translate(-236,-189);
+    changeCanv();
+}
+
+ function e1(){
+    var x = document.getElementById("i1").value;
+    var y = document.getElementById("i11").value;
+    // canvCont.resetTransform();
+    canvCont.clearRect(0,0,472,378);
+    canvCont.scale(x,y)
+    changeCanv();
+}
+ function e2(){
+    // canvCont.resetTransform();
+    canvCont.clearRect(0,0,472,378);
+    var x = document.getElementById("i2").value;
+    var y = document.getElementById("i22").value;
+    canvCont.setTransform(1,y,x,1,0,0);
+    changeCanv();
+}
+ function e3(){
+    var x = document.getElementById("i3").value;
+    // canvCont.resetTransform();
+    canvCont.clearRect(0,0,472,378);
+    canvCont.filter = "blur("+x+"px)";
+    changeCanv();
+}
+ function e4(){
+    var a1 = document.getElementById("i40").value;
+    var a2 = document.getElementById("i41").value;
+    var a3 = document.getElementById("i42").value;
+    var a4 = document.getElementById("i43").value;
+    var a5 = document.getElementById("i44").value;
+    var a6 = document.getElementById("i45").value;
+    canvCont.clearRect(0,0,472,378);
+    canvCont.setTransform(a1,a2,a3,a4,a5,a6);
+    changeCanv();
+
+}
+ function e5(){
+    var a1 = document.getElementById("i50").value;
+    var a2 = document.getElementById("i51").value;
+    var canvas = document.getElementById("canvas");
+    canvas.style.transformStyle = 'preserve-3d';
+    canvas.style.transform = "perspective("+a1+"px) rotateX("+a2+"deg)";
+}
+ function e6(){
+    canv.style.filter = "blur(1px)";
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn') && event.target.tagName != "A" && event.target.tagName != "INPUT") {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
